3b11775544595dfa5fc340e113f447d3baed8c5b
deevhyne1 <divinechukwueme77@gmail.com>
2023-02-17 04:32:33 +0100

    Return:  Always 0 (Succcess)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry
+ * Return:  Always 0 (Succcess)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"ogramming is like building a multilingual puzzle");
 	return (0);
 
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - "Entr"y
+ * Return:   Always 0 (Succcess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n ");
+	return (0);
+}
