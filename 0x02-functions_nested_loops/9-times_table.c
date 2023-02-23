75a0e190496d48c8d01924947540298f19338936
deevhyne1 <divinechukwueme77@gmail.com>
2023-02-23 02:59:55 +0100

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,6 +15,7 @@ void times_table(void)
 			if (result < 10)
 			{
 				_putchar(' ');
+				_putchar(',');
 				_putchar('0' + result);
 			}
 			else
@@ -22,7 +23,6 @@ void times_table(void)
 				_putchar('0' + (result / 10));
 				_putchar('0' + (result % 10));
 			}
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
