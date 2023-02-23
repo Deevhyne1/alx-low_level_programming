2cedd982674b7d7e86d580d76c7b842515000742
deevhyne1 <divinechukwueme77@gmail.com>
2023-02-23 03:18:53 +0100

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,12 +10,13 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			int result = i * j;
 
 			if (result < 10)
 			{
-				_putchar('0' + result);
-				_putchar(',');
 				_putchar(' ');
 			}
 			else
